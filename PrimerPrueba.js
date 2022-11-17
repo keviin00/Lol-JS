a5b80cd5fc3a54a5f63ecfef00e3525ff34669ab
keviin00 <90279937+keviin00@users.noreply.github.com>
2022-11-17 09:26:47 -0300

    Add files via upload

---
+++ PrimerPrueba.js
@@ -0,0 +1,4 @@
+let entrada = prompt("Ingresa el nombre de tu main");
+let entrada2 = prompt("Ingresa tu carril preferido");
+let salida  = "tu main es "+entrada + " y tu carril es  "+ entrada2 + ", " + "Asi que te gusta que te peguen";
+alert(salida);
